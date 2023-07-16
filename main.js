b8c08be17702e0725c753d026c9570d2cf6fcb0d
aeiao <114729153+aeiaooo@users.noreply.github.com>
2023-07-17 00:49:17 +0800

    Add files via upload
    Initial Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  }
