e52ff97e274fffae6256b3d70ab7578551db06b8
Jake Viado <jakeviado434@gmail.com>
2023-08-09 02:33:35 +0800

    hehe

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-function myFunction() {
-    var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  }
+function myFunction(dropdownNumber) {
+   const dropdown = document.querySelector(`#dropdown${dropdownNumber}`);
 
+   if (dropdown.style.display === "block") {
+     dropdown.style.display = "none";
+   } else {
+    dropdown.style.display = "block";
+   }
+}
 
-fetch('https://catfact.ninja/breeds',{
-    method: 'GET',
-  })
-  .then((response) => {
-    if (response.ok){
-      console.log('Success')
-    } else {
-      console.log('Not Successful')
-    }
-  response.json() 
+ 
+  
+
+fetch('https://catfact.ninja/breeds', {
+  method: 'GET',
 })
+  .then((response) => {
+    if (response.ok) {
+      console.log('Success');
+      return response.json();
+    } else {
+      console.log('Not Successful');
+    }
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error('An error occurred:', error);
+  });
